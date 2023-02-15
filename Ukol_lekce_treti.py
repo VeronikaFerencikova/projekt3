b6c2adfa6d1bb6d3fd62e11b4ca78699097a7690
Veronika Ferencikova <V.Strbikova@seznam.cz>
2023-02-15 15:24:47 +0100

    Vytvoren ukol treti v python.

--- Ukol_lekce_treti.py
+++ Ukol_lekce_treti.py
@@ -2,3 +2,6 @@ print("Ahoj")
 
 jmeno = input("Jaké je tvoje jméno? ")
 print("Ahoj" + " " + jmeno)
+
+prijmeni = input("Jaké je tvoje přijmení? ")
+print("Vítám Vás" + " " + jmeno + " " + prijmeni)
